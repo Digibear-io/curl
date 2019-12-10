bf333cb7477d46f87b0fae2d879047d1ae480d1b
Lem Canady <lem.canady@gmail.com>
2019-12-10 01:17:34 -0800

    Added Readme

---
+++ README.md
@@ -0,0 +1,46 @@
+# @digibear/curl
+### A super simple curl utility written in typescript. 
+
+This was originally written for a series of shell scripts I wrote using node. 
+
+### ``` curl(address, options) ```
+
+
+```JS
+// ts-node
+import curl from "curl";
+
+// Node
+const curl = require("curl/dist/");
+
+(async () => {
+  const results = await curl("http://www.example.com", {
+    mode: "GET",
+    header: {
+      "api-key": "xxxxx-xxxxx-xxxxx",
+    },
+    flags: ["headers"]
+  })
+})().catch(err => {
+  console.error(err);
+  process.exit(1)
+})
+```
+
+## Options
+* `mode(string)`
+* `headers(Object)`
+  
+  * `key: value`
+
+* `user(Object)`
+
+  * `user`
+
+  * `password`
+
+* `body(Object)`
+
+  * `key: value`
+
+* `flags(array)`
---
+++ dist/index.js
@@ -0,0 +1,22 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const child_process_1 = require('child_process');
+/**
+ * Make a curl request to a remote server.
+ * @param address The address to send the curl request to.
+ * @param options Optional params that can be sent with the request
+ */
+exports.default = async (address, { mode = 'GET', headers = {}, user, data, flags = [] }) => {
+	const reqHeaders = Object.keys(headers).reduce((acc, curr) => (acc += `-H "${curr}: ${headers[curr]}"`), '');
+	// Build the curl string.
+	let curlString = `curl -X ${mode} ${reqHeaders} ${data ? '-D ' + JSON.stringify(data) : ''} ${user
+		? '--user' + user.user + ':' + user.password
+		: ''} ${flags.map((flag) => '--' + flag).join(' ')} ${address}`;
+	// Execute the curl, and
+	return new Promise((resolve, reject) => {
+		child_process_1.exec(curlString, (err, stdout) => {
+			if (err) reject(err);
+			return resolve(stdout);
+		});
+	});
+};
