83e2c33001f80b3e197a00f29ecc28b252691d35
= <=>
2020-01-19 22:39:30 -0800

    Bug fix

--- src/curl.ts
+++ src/curl.ts
@@ -1,19 +1,19 @@
-import { exec } from 'child_process';
+import { exec } from "child_process";
 
 interface CurlOptions {
-	mode?: 'GET' | 'PUT' | 'PUSH' | 'DELETE' | 'POST';
-	headers?: {
-		apiKey?: string;
-		[key: string]: any;
-	};
-	user?: {
-		user: string;
-		password: string;
-	};
-	data?: {
-		[key: string]: any;
-	};
-	flags?: string[];
+  mode?: "GET" | "PUT" | "PUSH" | "DELETE" | "POST";
+  headers?: {
+    apiKey?: string;
+    [key: string]: any;
+  };
+  user?: {
+    user: string;
+    password: string;
+  };
+  data?: {
+    [key: string]: any;
+  };
+  flags?: string[];
 }
 
 /**
@@ -22,24 +22,26 @@ interface CurlOptions {
  * @param options Optional params that can be sent with the request
  */
 export default async (
-	address: string,
-	{ mode = 'GET', headers = {}, user, data, flags = [] }: CurlOptions
+  address: string,
+  { mode = "GET", headers = {}, user, data, flags = [] }: CurlOptions
 ): Promise<string> => {
-	const reqHeaders = Object.keys(headers).reduce(
-		(acc: string, curr: string) => (acc += `-H "${curr}: ${headers[curr]}"`),
-		''
-	);
+  const reqHeaders = Object.keys(headers).reduce(
+    (acc: string, curr: string) => (acc += `-H "${curr}: ${headers[curr]}"`),
+    ""
+  );
 
-	// Build the curl string.
-	let curlString = `curl -X ${mode} ${reqHeaders} ${data ? '-D ' + JSON.stringify(data) : ''} ${user
-		? '--user' + user.user + ':' + user.password
-		: ''} ${flags.join(' ')} ${address}`;
+  // Build the curl string.
+  let curlString = `curl -X ${mode} ${reqHeaders} ${
+    data ? "-D " + JSON.stringify(data) : ""
+  } ${user ? "--user" + user.user + ":" + user.password : ""} ${flags.join(
+    " "
+  )} ${address}`;
 
-	// Execute the curl, and
-	return new Promise((resolve, reject) => {
-		exec(curlString, (err, stdout) => {
-			if (err) reject(err);
-			return resolve(stdout);
-		});
-	});
+  // Execute the curl, and
+  return new Promise((resolve, reject) => {
+    exec(curlString, (err, stdout) => {
+      if (err) reject(err);
+      return resolve(stdout);
+    });
+  });
 };
